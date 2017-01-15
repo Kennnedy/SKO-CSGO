eb6781af51466965c62f0d1584a9dbb921406e4d
JordanS0 <JordanS0@DESKTOP-2BFBLV0>
2017-01-14 19:27:30 -0800

    Add project files.

---
+++ MetaInfo.h
@@ -0,0 +1,11 @@
+/*
+Syn's AyyWare Framework
+*/
+
+#pragma once
+
+#define AYYWARE_META_GAME "Counter-Strike: Global Offensive"
+#define AYYWARE_META_CHEATVER "1.0.0"
+#define AYYWARE_META_CHEATNAME "BITWare for Counter-Strike: Global Offensive"
+
+void PrintMetaHeader();
